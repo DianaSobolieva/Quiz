9d01788ad6e0235f76e800910d64931fcc9f65bd
Glitch (personality-quiz-starter-code) <none>
2020-07-23 16:06:02 +0000

    🏀🎐 Checkpoint ./index.html:19448333/3887 ./script.js:19448333/11

--- script.js
+++ script.js
@@ -27,10 +27,10 @@ var q3a2 = document.getElementById("q3a2");
 var q3a3 = document.getElementById("q3a3");
 var q3a4 = document.getElementById("q3a4");
 
-var q4a1 = document.getElementById("q4a1");
-var q4a2 = document.getElementById("q4a2");
-var q4a3 = document.getElementById("q4a3");
-var q4a4 = document.getElementById("q4a4");
+var q4a1 = document.getElementById("q4a1-b");
+var q4a2 = document.getElementById("q4a2-s");
+var q4a3 = document.getElementById("q4a3-sk");
+var q4a4 = document.getElementById("q4a4-w");
 
 
 
